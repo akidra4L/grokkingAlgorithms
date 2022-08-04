92d5a4225070c4cc6c6f0647110f1160eea7826d
Alikhan Gubayev <jonasofan.gubaev@gmail.com>
2022-08-04 11:09:40 +0600

    algo

---
+++ 01_Introduction/binarySearch.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int binarySearch(vector <int> array, int number)
+{
+    int left = 0, right = array.size() - 1;
+    while(left <= right) {
+        int mid = (left + right) / 2;
+        if(array[mid] == number) {
+            return mid;
+        }
+        else if(array[mid] < number) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    vector <int> array;
+    array.push_back(1);
+    array.push_back(3);
+    array.push_back(5);
+    array.push_back(7);
+    array.push_back(9);
+
+    cout << binarySearch(array, 5) << endl;
+    cout << binarySearch(array, -1) << endl;
+
+    return 0;
+}
